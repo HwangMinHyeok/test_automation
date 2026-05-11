2bde6857a9b3ab11bbf0b12f8381fc3da376b5e3
HwangMinHyeok <aldur930@gmail.com>
2026-05-11 12:00:35 +0900

    feat: 여러 상품 추가 확인

--- tests/features/cart.feature
+++ tests/features/cart.feature
@@ -4,3 +4,8 @@ Feature: Cart
         Given 사용자가 Products 페이지에 있다
         When add to cart 버튼을 눌러 상품을 카트에 추가한다
         Then 카트에 해당 상품이 들어있다
+
+    Scenario: 여러 상품 카트 추가 확인
+        Given 사용자가 Products 페이지에 있다
+        When add to cart 버튼을 눌러 여러 상품을 카트에 추가한다
+        Then 카트에 해당 상품들이 모두 들어있다
--- tests/steps/cart_steps.py
+++ tests/steps/cart_steps.py
@@ -24,3 +24,34 @@ def add_product_to_cart(products_page, shared_data):
 def expect_product_in_cart(products_page, shared_data):
     product_info = shared_data["product"]
     ViewCartPage(products_page).expect_product_exists(product_info)
+    
+
+# Scenario: 여러 상품 카트 추가 확인
+@given("사용자가 Products 페이지에 있다")
+def given_products_page():
+    pass
+
+@when("add to cart 버튼을 눌러 여러 상품을 카트에 추가한다")
+def add_multiple_products_to_cart(products_page, shared_data):
+    # 테스트의 안정성을 위해 항상 첫 번째, 두 번째, 열 번째 상품을 추가하도록
+    indices = [0, 1, 9]
+    
+    shared_data["products"] = []
+    
+    for index in indices[:-1]:
+        # 추가한 상품 정보를 shared_data에 저장 
+        shared_data["products"].append(ProductsPage(products_page).product(index).get_product_info())
+        
+        # 상품을 카트에 추가하고 Continue Shopping 버튼 클릭하여 계속 쇼핑
+        ProductsPage(products_page).add_product_to_cart(index)
+        ProductsPage(products_page).CART_MODAL.click_continue_shopping()
+    
+    # 상품을 카트에 추가하고 View Cart 버튼 클릭하여 카트 페이지로 이동
+    ProductsPage(products_page).add_product_to_cart(indices[-1])    
+    ProductsPage(products_page).CART_MODAL.click_view_cart()
+
+
+@then("카트에 해당 상품들이 모두 들어있다")
+def expect_multiple_products_in_cart(products_page, shared_data):
+    for product_info in shared_data["products"]:
+        ViewCartPage(products_page).expect_product_exists(product_info)
